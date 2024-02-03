c1a86c143d28aa015b05310710d2472c57f48700
traycer255 <pavlyutenkovaleksey@yandex.ru>
2024-02-04 00:28:01 +0500

    Добавил все элементы из верстки в форму пассажиров

--- script/modules/formsPerson.js
+++ script/modules/formsPerson.js
@@ -29,6 +29,27 @@ const createFormPerson = (i) => {
     required: 'true',
   });
 
+  const fieldTicket = createElement('div', {
+    className: 'field',
+  });
+
+  const labelTicket = createElement('label', {
+    className: 'field__label',
+    textContent: 'Номер билета (10 цифр)',
+    for: `ticket${i}`,
+  });
+
+  const inputTicket = createElement('input', {
+    className: 'field__input',
+    id: `ticket${i}`,
+    name: 'ticket',
+    type: 'text',
+    placeholder: 'Номер билета',
+    minLength: 10,
+    maxLength: 10,
+    required: 'true',
+  });
+
   const button = createElement('button', {
     className: 'btn-confirm',
     type: 'submit',
@@ -36,30 +57,18 @@ const createFormPerson = (i) => {
   })
 
   fieldName.append(labelName, inputName)
-  form.append(title, fieldName, button)
+  fieldTicket.append(labelTicket, inputTicket)
+
+
+  form.append(title, fieldName, fieldTicket, button)
 
   return form;
 };
 
-/*
-<h2 class="person__title">Пассажир #1</h2>
-
-<div class="field">
-<label class="field__label">ФИО</label>
-<input class="field__input" id="name0" name="name" type="text" placeholder="Введите ваше ФИО" required="">
-</div>
-
-<div class="field">
-<label class="field__label">Номер билета (10 цифр)</label>
-<input class="field__input" id="ticket0" name="ticket" type="text" placeholder="Номер билета" required="" minlength="10" maxlength="10">
-</div>
-
-<button class="btn-confirm" type="submit">Подтвердить</button>
-*/
 
 const getFormsPerson = (count) => {
   const forms = [];
-
+  if (count > 6) count = 6;
   for (let i = 0; i < count; i++) {
     forms.push(createFormPerson(i))
   }
