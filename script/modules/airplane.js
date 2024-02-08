e4f7d6258b7b152b980c20b2890b3f769d9d7165
traycer255 <pavlyutenkovaleksey@yandex.ru>
2024-02-08 18:52:01 +0500

    Создал самолет в js

--- script/modules/airplane.js
+++ script/modules/airplane.js
@@ -1,5 +1,57 @@
 import createElement from "./createElement.js";
 
+// создает ряды с сиденьями самолета
+const createBlockSeat = (n, count) => {
+  // буквы колонок
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+  const fuselage = createElement('ol', {
+    className: 'fuselage',
+  });
+
+  for (let i = n; i < count + n; i++) {
+    // создаю ряды
+    const wrapperRowLi = createElement('li');
+    //создаю списки для колонок
+    const seatRowOl = createElement('ol', {
+      className: 'seats',
+    });
+
+    // создаю колонки и кресла
+    const columns = letters.map(letter => {
+      const columnLi = createElement('li', {
+        className: 'seat',
+      });
+
+      const wrapperSeat = createElement('label');
+      const seat = createElement('input', {
+        name: 'seat',
+        type: 'checkbox',
+        value: `${i + letter}`,
+      });
+
+      wrapperSeat.append(seat);
+      columnLi.append(wrapperSeat);
+      return columnLi;
+    });
+
+
+    // вставляю колонки в обертку ol 
+    seatRowOl.append(...columns);
+
+
+    // вставляю списки для колонок в ряды
+    wrapperRowLi.append(seatRowOl);
+
+    fuselage.append(wrapperRowLi);
+
+  };
+
+  return fuselage;
+
+};
+
+// создает ряд с выходом
 const createExit = () => {
   const fuselage = createElement('div', {
     className: 'exit fuselage',
@@ -44,12 +96,18 @@ const createAirplane = (title, scheme) => {
   // нос самолета
   const cockpit = createCockpit(title);
 
+  let n = 1;
   // корпус
   const elements = scheme.map((type) => {
     if (type === 'exit') {
       return createExit();
     };
 
+    if (typeof type === 'number') {
+      const blockSeats = createBlockSeat(n, type);
+      n = n + type;
+      return blockSeats;
+    };
 
   });
 
