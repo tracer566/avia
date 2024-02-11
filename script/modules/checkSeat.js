98e8edd4e25eb2fe054c0b5d904f54b0e5c373a8
traycer255 <pavlyutenkovaleksey@yandex.ru>
2024-02-11 23:36:42 +0500

    Добавил еще файл

---
+++ script/modules/checkSeat.js
@@ -0,0 +1,8 @@
+// проверяет сколько мест можно забронировать
+const checkSeat = (form, data) => {
+  form.addEventListener('change', (e) => {
+    console.log('change', e);
+  });
+};
+
+export default checkSeat;
