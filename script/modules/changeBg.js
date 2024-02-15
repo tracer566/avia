8a543b5cf6d1275d6380cd3d72ba3c1d58ebbef9
traycer255 <pavlyutenkovaleksey@yandex.ru>
2024-02-15 22:12:14 +0500

    Сделал моб картинки

--- script/modules/changeBg.js
+++ script/modules/changeBg.js
@@ -1,23 +1,44 @@
 const changeTourBg = (tourData) => {
   console.log('tourData: ', tourData);
   let bg
-  switch (+tourData.id) {
-    case 1:
-      bg = "url('img/bg/maldives.jpg')";
-      break;
-    case 2:
-      bg = "url('img/bg/india.jpg')";
-      break;
-    case 3:
-      bg = "url('img/bg/egypt.jpg')";
-      break;
-    case 4:
-      bg = "url('img/bg/turkey.jpg')";
-      break;
-    case 5:
-      bg = "url('img/bg/sochi.jpg')";
-      break;
-  }
+
+  if (document.documentElement.clientWidth < 480) {
+    switch (+tourData.id) {
+      case 1:
+        bg = "url('img/mob/maldives-mob.jpg')";
+        break;
+      case 2:
+        bg = "url('img/mob/india-mob.jpg')";
+        break;
+      case 3:
+        bg = "url('img/mob/egypt-mob.jpg')";
+        break;
+      case 4:
+        bg = "url('img/mob/turke-moby.jpg')";
+        break;
+      case 5:
+        bg = "url('img/mob/sochi-mob.jpg')";
+        break;
+    }
+  } else {
+    switch (+tourData.id) {
+      case 1:
+        bg = "url('img/bg/maldives.jpg')";
+        break;
+      case 2:
+        bg = "url('img/bg/india.jpg')";
+        break;
+      case 3:
+        bg = "url('img/bg/egypt.jpg')";
+        break;
+      case 4:
+        bg = "url('img/bg/turkey.jpg')";
+        break;
+      case 5:
+        bg = "url('img/bg/sochi.jpg')";
+        break;
+    }
+  };
 
   document.body.style.backgroundImage = bg;
 
